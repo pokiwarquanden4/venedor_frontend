62ed597a5d716da0a3aed17860b114c4104fdddc
TranMinhQuang2k2 <quangminhtranq@gmail.com>
2023-06-27 13:13:44 +0700

    edit click out side

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,9 +1,23 @@
+import { useEffect, useRef } from 'react';
 import styles from './Popup.module.scss';
 
 function Popup({ width = '800px', height = '500px', children, onClick, right, highestZIndex }) {
+  const backGroundRef = useRef();
+  useEffect(() => {
+    const handleClickOutSide = (e) => {
+      if (backGroundRef.current.contains(e.target)) {
+        onClick();
+      }
+    };
+    document.addEventListener('click', handleClickOutSide);
+    return () => {
+      document.removeEventListener('click', handleClickOutSide);
+    };
+  });
+
   return (
     <div className={`${styles.wrapper} ${highestZIndex ? styles.upper : ''}`}>
-      <div className={styles.backGround}></div>
+      <div className={styles.backGround} ref={backGroundRef}></div>
       <div className={`${styles.content} ${right ? styles.right : ''}`} style={{ width, height }}>
         {children}
         <div className={styles.exit_button} onClick={onClick}>
